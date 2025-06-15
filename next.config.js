8bcdf747c4774787906345773b40eee6ae770f11
sturge87 <sturgess1987@gmail.com>
2025-06-15 12:25:40 +0700

    Update project configuration and add Prisma models

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  images: {
+    domains: ['images.unsplash.com', 'maps.googleapis.com'],
+  },
+  experimental: {
+    serverActions: true,
+  },
+  webpack: (config) => {
+    config.externals = [...config.externals, { 'utf-8-validate': 'commonjs utf-8-validate', 'bufferutil': 'commonjs bufferutil' }];
+    return config;
+  },
 };
 
 module.exports = nextConfig; 
