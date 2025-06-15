1a7483043b65ad07a387e5d6b0cc7fe244f02a40
sturge87 <sturgess1987@gmail.com>
2025-06-15 12:26:28 +0700

    Remove Prisma and clean up configuration for Supabase

--- next.config.js
+++ next.config.js
@@ -1,15 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['images.unsplash.com', 'maps.googleapis.com'],
+    domains: ['images.unsplash.com'],
   },
   experimental: {
     serverActions: true,
-  },
-  webpack: (config) => {
-    config.externals = [...config.externals, { 'utf-8-validate': 'commonjs utf-8-validate', 'bufferutil': 'commonjs bufferutil' }];
-    return config;
-  },
+  }
 };
 
 module.exports = nextConfig; 
